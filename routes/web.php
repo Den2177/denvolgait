4faac9c00ce41fa97d364c7c0e2bb8a6ea3ea8d8
Den2177 <jdenmail.com@gmail.com>
2022-09-16 17:00:51 +0300

    added changes

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/getProducts/{productId}',[\App\Http\Controllers\ProductController::class, 'store']);
+Route::get('/getProducts/{productId}',[\App\Http\Controllers\ProductController::class, 'show']);
